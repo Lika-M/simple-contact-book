4277a44c72738bae0d74af8da4e6b9715d11a774
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-04-08 18:25:45 +0300

    Adding Header and Footer.

---
+++ src/features/common/footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <footer>React-redux-app &copy; 2023</footer>
+    );
+}
+
+export default Footer;
---
+++ src/features/common/header/Header.js
@@ -0,0 +1,8 @@
+import './Header.css'
+const Header = ()=> {
+    return (
+        <header>Contact Book &#x02710;</header>
+    );
+}
+
+export default Header;
