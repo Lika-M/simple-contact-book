19682c9a0ab2fd23c8b5cba51aff425d4c933f35
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-04-11 13:50:16 +0300

    Adding reducers to add and update contacts.

--- src/features/contacts/catalog/catalogSlice.js
+++ src/features/contacts/catalog/catalogSlice.js
@@ -1,13 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice} from "@reduxjs/toolkit";
 import data from '../../../app/data.json';
 
 export const catalogSlice = createSlice({
     name: 'contacts',
     initialState: data,
     reducers: {
-       
+        addContact(state, action) {
+            state.push(action.payload);
+        },
+        updateContact(state, action) {
+            const index = state.findIndex(x => x.id === action.payload.id);
+            state.splice(index, 1, action.payload);
+        }
     }
 });
 
 export default catalogSlice.reducer;
-export const {viewContact, addContact, updateContact } = catalogSlice.actions;
+export const { addContact, updateContact } = catalogSlice.actions;
+
+
+
