c7ccd65dc58ee85fc42dabd244cbffffad488d60
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-04-11 13:40:53 +0300

    Fixing.

--- src/features/contacts/details/Details.js
+++ src/features/contacts/details/Details.js
@@ -3,17 +3,13 @@ import './Details.css';
 
 const Details = ({ person }) => {
 
-    if (!person) {
-        return;
-    }
-
     return (
         <>
             <h2>Person details</h2>
             <div className="content">
                 <div className="info">
                     <div className="image col">
-                        <img src={person.picture} alt="" />
+                        <img src={person.picture} alt="Person img" />
                     </div>
                     <div className="col">
                         <h3 className="name">{person.firstName}</h3>
@@ -24,13 +20,13 @@ const Details = ({ person }) => {
                     <p className="info-line">&#9742; {person.phone}</p>
                     <p className="info-line">&#9993; {person.email}</p>
                     <div>
-                        <Link to="#" className="btn">&#9998;</Link>
+                        <Link to={`/contacts/edit/${person.id}`} className="btn">&#9998;</Link>
                         <button className="btn">&#10006;</button>
                     </div>
                 </div>
             </div>
             <div className="btn">
-                <button>Add contact</button>
+                <Link to={'/contacts/add'}>Add contact</Link>
             </div>
         </>
     );
