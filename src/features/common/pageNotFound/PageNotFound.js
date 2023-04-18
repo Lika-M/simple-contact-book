09a0ba388bf599180e7ad2dd18f2cf8655673157
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-04-18 20:37:16 +0300

    Adding PageNotFound.

---
+++ src/features/common/pageNotFound/PageNotFound.js
@@ -0,0 +1,17 @@
+import './PageNotFound.css'
+
+const PageNotFound = ({ error }) => {
+    return (
+        <section className="not-found-page">
+            {error &&
+                <div>
+                    {error.code && <h1>Error: {error.code}</h1>}
+                    {!error.code && <h1>Error: 400</h1>}
+                    <h2>{error.message}</h2>
+                    <p>The Page you are looking for doesn't exist or another error occurred. Go to <a href="/">Home page</a>.</p>
+                </div>}
+        </section>
+    );
+}
+
+export default PageNotFound;
