b3932f95ec80b7ea75992632fd03ce0a0465c33c
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2023-05-24 12:31:44 +0300

    Replace SVG icons and HTML symbols with icons from the @heroicons/react library.

--- src/features/contacts/details/Details.js
+++ src/features/contacts/details/Details.js
@@ -1,5 +1,7 @@
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { TrashIcon, XMarkIcon, PencilIcon, PhoneIcon, EnvelopeIcon} from '@heroicons/react/24/solid'
+import { MailIcon } from '@heroicons/react/outline';
 
 import { selectContactById } from '../catalog/catalogSlice.js';
 import { removeContact } from '../../../services/contactService.js';
@@ -24,13 +26,8 @@ const Details = ({ isLoading, resetId }) => {
         <>
             {!isLoading && <>
                 <h2>Person details
-                    <Link to={'/contacts'}>
-                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="xmark"
-                            className="svg-inline--fa fa-xmark" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                            <path fill="currentColor"
-                                d="M310.6 361.4c12.5 12.5 12.5 32.75 0 45.25C304.4 412.9 296.2 416 288 416s-16.38-3.125-22.62-9.375L160 301.3L54.63 406.6C48.38 412.9 40.19 416 32 416S15.63 412.9 9.375 406.6c-12.5-12.5-12.5-32.75 0-45.25l105.4-105.4L9.375 150.6c-12.5-12.5-12.5-32.75 0-45.25s32.75-12.5 45.25 0L160 210.8l105.4-105.4c12.5-12.5 32.75-12.5 45.25 0s12.5 32.75 0 45.25l-105.4 105.4L310.6 361.4z">
-                            </path>
-                        </svg>
+                    <Link className="info-close-btn" to={'/contacts'}>
+                        <XMarkIcon />
                     </Link>
                 </h2>
                 <div className="content">
@@ -44,25 +41,14 @@ const Details = ({ isLoading, resetId }) => {
                         </div>
                     </div>
                     <div className="info-details ">
-                        <p className="info-line">&#9742; {person.phone}</p>
-                        <p className="info-line">&#9993; {person.email}</p>
+                        <p className="info-line"><PhoneIcon /> {person.phone}</p>
+                        <p className="info-line"> <EnvelopeIcon/> {person.email}</p>
                         <div>
-                            <Link to={`/contacts/edit/${person.objectId}`} className="btn">
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="pen-to-square"
-                                    className="svg-inline--fa fa-pen-to-square" role="img" xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 532 512">
-                                    <path fill="currentColor"
-                                        d="M490.3 40.4C512.2 62.27 512.2 97.73 490.3 119.6L460.3 149.7L362.3 51.72L392.4 21.66C414.3-.2135 449.7-.2135 471.6 21.66L490.3 40.4zM172.4 241.7L339.7 74.34L437.7 172.3L270.3 339.6C264.2 345.8 256.7 350.4 248.4 353.2L159.6 382.8C150.1 385.6 141.5 383.4 135 376.1C128.6 370.5 126.4 361 129.2 352.4L158.8 263.6C161.6 255.3 166.2 247.8 172.4 241.7V241.7zM192 63.1C209.7 63.1 224 78.33 224 95.1C224 113.7 209.7 127.1 192 127.1H96C78.33 127.1 64 142.3 64 159.1V416C64 433.7 78.33 448 96 448H352C369.7 448 384 433.7 384 416V319.1C384 302.3 398.3 287.1 416 287.1C433.7 287.1 448 302.3 448 319.1V416C448 469 405 512 352 512H96C42.98 512 0 469 0 416V159.1C0 106.1 42.98 63.1 96 63.1H192z">
-                                    </path>
-                                </svg>
+                            <Link to={`/contacts/edit/${person.objectId}`} className="btn edit">
+                                <PencilIcon />
                             </Link>
-                            <button className="btn" onClick={onDelete}>
-                                <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="trash"
-                                    className="svg-inline--fa fa-trash" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 498 512">
-                                    <path fill="currentColor"
-                                        d="M135.2 17.69C140.6 6.848 151.7 0 163.8 0H284.2C296.3 0 307.4 6.848 312.8 17.69L320 32H416C433.7 32 448 46.33 448 64C448 81.67 433.7 96 416 96H32C14.33 96 0 81.67 0 64C0 46.33 14.33 32 32 32H128L135.2 17.69zM394.8 466.1C393.2 492.3 372.3 512 346.9 512H101.1C75.75 512 54.77 492.3 53.19 466.1L31.1 128H416L394.8 466.1z">
-                                    </path>
-                                </svg>
+                            <button className="btn close" onClick={onDelete}>
+                                <TrashIcon />
                             </button>
                         </div>
                     </div>
